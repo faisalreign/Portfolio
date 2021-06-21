66ae8ac71a60c39671fb0663d322ea1cc49dfc7e
faisalreign <68537434+faisalreign@users.noreply.github.com>
2021-06-21 12:07:44 -0700

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+
+namespace WebApplication3Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult portfolio()
+        {
+            return View();
+        }
+
+        public ActionResult contact()
+        {
+            return View();
+        }
+    }
+}
